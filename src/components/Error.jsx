935b829dda3ba24f7a50a89ce1b43d6dfc17442c
Babatunde-Ben <babatundeben110.com>
2022-05-09 02:26:17 +0100

    modified error page stylings

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -4,23 +4,18 @@ import "../css/error.css";
 import errorImage from "../img/error.svg";
 
 const Error = () => {
-	return (
-		<div>
-			<div className="error">
-				<div className="error-wrap">
-					<img src={errorImage} className="errorImage" alt="" />
-					<h2 className="error-text">Error</h2>
-				</div>
-				<p className="pg-not-fd">Page Not Found</p>
-
-				<div className="back-home-wrap">
-					<Link to="/" className="back-home">
-						Click to go back to Home Page
-					</Link>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <section
+      className="error-section
+	"
+    >
+      <img src={errorImage} className="errorImage" alt="" />
+      <h3 className="error-message">Page not found</h3>
+      <Link to="/" className="back-home">
+        back to home page
+      </Link>
+    </section>
+  );
 };
 
 export default Error;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-import "../css/navbar1.css";
+import "../css/navbar.css";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
