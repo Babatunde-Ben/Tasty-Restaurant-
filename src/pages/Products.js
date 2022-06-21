2513eb5e22c43f098e2b6ff01bf48af35fc0ad5e
Babatunde-Ben <babatundeben110@gmail.com>
2022-06-21 02:01:47 +0100

    mofidied product items

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -2,78 +2,79 @@ import React from "react";
 // import { Outlet } from "react-router-dom";
 
 // Component import
-import meal1 from "../img/meal-1.jpg";
-import meal2 from "../img/meal-2.jpg";
-import meal3 from "../img/meal-3.jpg";
-import meal4 from "../img/meal-4.jpg";
-import meal5 from "../img/meal-5.jpg";
-import meal6 from "../img/meal-6.jpg";
-import meal7 from "../img/meal-7.jpg";
-import meal8 from "../img/meal-8.jpg";
-import meal9 from "../img/meal-9.jpg";
+// import meal1 from "../img/meal-1.jpg";
+import meal1 from "../img/Sweet_and_Sticky_Chicken_Strips.jpg";
+import meal2 from "../img/Fruit_and_Yogurt_Parfait.jpg";
+import meal3 from "../img/Toasted_Agnolotti.jpg";
+import meal4 from "../img/Asian_Shrimp_Stir-Fry.jpg";
+import meal5 from "../img/Tomato_Basil_Chicken.jpg";
+import meal6 from "../img/Cooker_Lamb_Curry.jpg";
+import meal7 from "../img/BBQ_Mac_and_Cheese.jpg";
+import meal8 from "../img/Salmon_with_roasted_vegetables.jpg";
+import meal9 from "../img/Chicken_Piccata_with_Artichokes.jpg";
 
 import "../css/products.css";
 
 const productDetails = [
   {
     id: 1,
-    name: "Meal 1",
+    name: "Sweet and Sticky Chicken Strips",
     image: meal1,
     price: 2300,
     desc: " Lorem ipsum dolor sit amet conse ctetur adipisi cing elit. Dolor cons equuntu",
   },
   {
     id: 2,
-    name: "Meal 2",
+    name: "Fruit and Yogurt Parfait",
     image: meal2,
     price: 1800,
     desc: " Lorem ipsum dolor sit amet conse ctetur adipisi cing elit. Dolor cons equuntu",
   },
   {
     id: 3,
-    name: "Meal 3",
+    name: "Toasted Agnolotti",
     image: meal3,
     price: 4100,
     desc: " Lorem ipsum dolor sit amet conse ctetur adipisi cing elit. Dolor cons equuntu",
   },
   {
     id: 4,
-    name: "Meal 4",
+    name: "Asian Shrimp Stir-Fry",
     image: meal4,
     price: 1500,
     desc: " Lorem ipsum dolor sit amet conse ctetur adipisi cing elit. Dolor cons equuntu",
   },
   {
     id: 5,
-    name: "Meal 5",
+    name: "Tomato Basil Chicken",
     image: meal5,
     price: 1150,
     desc: " Lorem ipsum dolor sit amet conse ctetur adipisi cing elit. Dolor cons equuntu",
   },
   {
     id: 6,
-    name: "Meal 6",
+    name: "Cooker Lamb Curry",
     image: meal6,
     price: 2100,
     desc: " Lorem ipsum dolor sit amet conse ctetur adipisi cing elit. Dolor cons equuntu",
   },
   {
     id: 7,
-    name: "Meal 7",
+    name: "BBQ Mac and Cheese",
     image: meal7,
     price: 2500,
     desc: " Lorem ipsum dolor sit amet conse ctetur adipisi cing elit. Dolor cons equuntu",
   },
   {
     id: 8,
-    name: "Meal 8",
+    name: "Salmon with vegetables",
     image: meal8,
     price: 2300,
     desc: " Lorem ipsum dolor sit amet conse ctetur adipisi cing elit. Dolor cons equuntu",
   },
   {
     id: 9,
-    name: "Meal 9",
+    name: "Chicken Piccata with Artichokes",
     image: meal9,
     price: 1000,
     desc: " Lorem ipsum dolor sit amet conse ctetur adipisi cing elit. Dolor cons equuntu",
